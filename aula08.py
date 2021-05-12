0445aa36f48a89da870f8f54d4f3e72f50f08bb4
richardcostarocha <richard.ufpa2017@gmail.com>
2021-05-12 16:56:46 -0300

    second commit

--- aula08.py
+++ aula08.py
@@ -1,1 +1,23 @@
-#test 
+from datetime import date
+def voto(AnoDeNascimento): 
+    anoAtual = date.today().year
+    idade = anoAtual - AnoDeNascimento
+    voto = ''
+    if idade < 0:
+        voto = 'data de nascimento invalido'
+    else:
+        if idade < 16:
+            voto = 'VOTO NEGADO'
+        elif idade < 18:
+            voto = 'VOTO OPCIONAL'
+        else:
+            voto = 'VOTO OBRIGATORIO'
+
+    return voto
+
+anoDeNascimento = int(input('quando foi que vc nasceu: '))
+voto = voto(anoDeNascimento)
+print(f'{voto}')
+
+
+    
