3d0ad6933b2c2aefdf3575313710b5c5e8216586
richardcostarocha <richard.ufpa2017@gmail.com>
2021-05-13 16:41:25 -0300

    .

---
+++ teste.py
@@ -0,0 +1,38 @@
+
+'''
+a = [[]]
+
+for l in range(0,3):
+    for c in range(0,3):
+        a[l][c] = int(input(f'digite um valor para [{l},{c}]: '))
+
+print('='*166)
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{a[l][c]}]', end='')
+    print()
+'''
+
+n_linhas = int(input('quantas linhas tem a matriz: '))
+n_colunas = int(input('quantas colunas tem a matriz: '))
+valor = int(input('qual o valor a ser preenchido: '))
+
+def crie_matriz(n_linhas, n_colunas, valor):
+    matriz = [] # lista vazia
+    for i in range(n_linhas):
+        # cria a linha i
+        linha = [] # lista vazia
+        for j in range(n_colunas):
+            linha.append(valor)
+
+        # coloque linha na matriz
+        matriz.append(linha)
+
+    return matriz
+a = crie_matriz(n_linhas, n_colunas, valor)
+for l in range(n_linhas):
+    for c in range(n_colunas):
+        print(f'[{a[l][c]}]', end='')
+    print()
+
